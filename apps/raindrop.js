3763f9996a9b6ca41844c3266c0f5d23982993a3
efosa <efosaokpugie@gmail.com>
2017-10-23 17:24:03 +0100

    remodified

--- apps/raindrop.js
+++ apps/raindrop.js
@@ -1,7 +1,11 @@
 const rainDrops = (num) => {
+// First of all check for invalid input
   if(typeof num !== 'number') {
     return 'Invalid Input Type'
   }
+/*
+* Initialize an object to look up information
+*/
   let outputDict = {3: "Pling", 5: "Plang", 7: "Plong"};
   let rainDropSpeak = '';
   if (num % 3 === 0){
